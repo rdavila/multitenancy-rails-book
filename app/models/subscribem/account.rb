2682570176d89e951f3e6d9c3de7ded246c36617
Ruben Davila <ruben.grunge84@gmail.com>
2014-07-17 12:44:38 -0500

    Validate subdomain name. page #29

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -1,6 +1,13 @@
 module Subscribem
   class Account < ActiveRecord::Base
+    EXCLUDED_SUBDOMAINS = %w(admin)
+
+    validates_exclusion_of :subdomain, :in => EXCLUDED_SUBDOMAINS, :message => "is not allowed. Please choose another subdomain."
+    validates_format_of :subdomain, :with => /\A[\w\-]+\Z/i, :message => "is not allowed. Please choose another subdomain."
     validates :subdomain, :presence => true, :uniqueness => true
+    before_validation do
+      self.subdomain = subdomain.to_s.downcase
+    end
 
     belongs_to :owner, :class_name => "Subscribem::User"
 
