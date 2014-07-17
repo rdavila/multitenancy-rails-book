c14332d5c296ea879d3ee9d344285d3a7cd7de5e
Ruben Davila <ruben.grunge84@gmail.com>
2014-07-17 11:58:10 -0500

    Add ability for subdomains to accounts. page #24

---
+++ app/controllers/subscribem/accounts_controller.rb
@@ -0,0 +1,29 @@
+require_dependency "subscribem/application_controller"
+
+module Subscribem
+  class AccountsController < ApplicationController
+
+    def new
+      @account = Subscribem::Account.new
+      @account.build_owner
+    end
+
+    def create
+      account = Subscribem::Account.create(account_params)
+
+      env["warden"].set_user(account.owner, :scope => :user)
+      env["warden"].set_user(account, :scope => :account)
+
+      flash[:success] = "Your account has been successfully created."
+      redirect_to subscribem.root_url(:subdomain => account.subdomain)
+    end
+
+    private
+      def account_params
+        params.require(:account).permit(:name, :subdomain, {:owner_attributes => [
+          :email, :password, :password_confirmation
+        ]})
+      end
+
+  end
+end
---
+++ spec/features/accounts/sign_up_spec.rb
@@ -0,0 +1,18 @@
+require"rails_helper"
+
+feature"Accounts"do
+  scenario "creating an account" do
+    visit subscribem.root_path
+    click_link "Account Sign Up"
+    fill_in "Name", :with => "Test"
+    fill_in "Subdomain", :with => "test"
+    fill_in "Email", :with => "subscribem@example.com"
+    fill_in "Password", :with => "password", exact: true
+    fill_in "Password confirmation", :with => "password"
+    click_button "Create Account"
+    success_message = "Your account has been successfully created."
+    expect(page).to have_content(success_message)
+    expect(page).to have_content("Signed in as subscribem@example.com")
+    expect(page.current_url).to eq("http://test.example.com/subscribem/")
+  end
+end
